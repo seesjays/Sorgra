17d4f65578e66313c9e59d744e67ee3cefc5a804
ChrisO <39662993+ChrisOh431@users.noreply.github.com>
2021-10-13 23:33:52 -0500

    data highlight func

--- src/scripts/dataset.ts
+++ src/scripts/dataset.ts
@@ -1,13 +1,28 @@
 import { ChartData } from "chart.js";
 
+
+enum HIGHLIGHT_TYPE {
+    BASE,
+    DISCREPANCY,
+    CORRECTED,
+    SEEKING,
+    SELECTED,
+}
+
+type HighlightedIndex = {
+    color: HIGHLIGHT_TYPE;
+    indices: number[];
+}
+
 class SortingDatasetModel {
     data_set_size: number;
     step_counter: number;
 
     data_x: number[];
 
-    data_original: number[];
-    data_y: number[];
+    data_original: number[]; // Returning to original dataset
+    data_y: number[]; // Actual sorting
+    data_y_displays: {[highlight in HIGHLIGHT_TYPE]: number[]}; // Highlight diffing
 
     algorithm_name: string;
 
@@ -19,8 +34,9 @@ class SortingDatasetModel {
         this.step_counter = 0;
 
         this.data_x = Array.from({length: 10}, (_, i) => i + 1);
-        this.data_original = [];
         this.data_y = this.generate_yvals();
+        this.data_original = [...this.data_y];
+        this.data_y_displays = {0: [...this.data_y], 1: [...this.data_y], 2: [...this.data_y], 3: [...this.data_y], 4: [...this.data_y]};
 
         this.algorithm_name = init_algorithm;
 
@@ -44,15 +60,53 @@ class SortingDatasetModel {
         return outarr;
     }
 
-    private generate_base_chart_dataset(): ChartData {    
+    private generate_base_chart_dataset(): ChartData {   
+        let base_col = "76, 114, 176"
+        let discrep_col = "196, 78, 82"
+        let corrected_col = "85, 168, 104"
+        let seeking_col = "76, 174, 255"
+        let selected_col = "204, 185, 116"
+
         return {
             labels: this.data_x,
             datasets: [
                 {
-                    label: "Base",
-                    data: this.data_y,
-                    backgroundColor: `rgba(76, 114, 176, 0.8)`,
-                    borderColor: `rgba(76, 114, 176, 1)`,
+                    label: "BASE",
+                    data: this.data_y_displays[HIGHLIGHT_TYPE.BASE],
+                    backgroundColor: `rgba(${base_col}, 0.8)`,
+                    borderColor: `rgba(${base_col}, 1)`,
+                    borderWidth: 2,
+                    barPercentage: 0.6,
+                },
+                {
+                    label: "DISCREPANCY",
+                    data: this.data_y_displays[HIGHLIGHT_TYPE.DISCREPANCY],
+                    backgroundColor: `rgba(${discrep_col}, 0.8)`,
+                    borderColor: `rgba(${discrep_col}, 1)`,
+                    borderWidth: 2,
+                    barPercentage: 0.6,
+                },
+                {
+                    label: "CORRECTED",
+                    data: this.data_y_displays[HIGHLIGHT_TYPE.CORRECTED],
+                    backgroundColor: `rgba(${corrected_col}, 0.8)`,
+                    borderColor: `rgba(${corrected_col}, 1)`,
+                    borderWidth: 2,
+                    barPercentage: 0.6,
+                },
+                {
+                    label: "SEEKING",
+                    data: this.data_y_displays[HIGHLIGHT_TYPE.SEEKING],
+                    backgroundColor: `rgba(${seeking_col}, 0.8)`,
+                    borderColor: `rgba(${seeking_col}, 1)`,
+                    borderWidth: 2,
+                    barPercentage: 0.6,
+                },
+                {
+                    label: "SELECTED",
+                    data: this.data_y_displays[HIGHLIGHT_TYPE.SELECTED],
+                    backgroundColor: `rgba(${selected_col}, 0.8)`,
+                    borderColor: `rgba(${selected_col}, 1)`,
                     borderWidth: 2,
                     barPercentage: 0.6,
                 },
@@ -61,8 +115,20 @@ class SortingDatasetModel {
     };
 
     // append colored dataset with specific indicies actually containing values, rest are 0
-    // TODO
-
+    // set everything but the indices to 0 for base, opposite for highlight
+    public highlight_dataset(highlights: HighlightedIndex[])
+    {
+        for (let highlight of highlights)
+        {
+            let selected_color = this.data_y_displays[highlight.color];
+            selected_color = new Array(this.data_set_size).fill(0);
+            for (let indice of highlight.indices)
+            {
+                selected_color[indice] = this.data_y[indice];
+                this.data_y_displays[HIGHLIGHT_TYPE.BASE][indice] = 0;
+            }
+        }
+    }
 }
 
 const mdl = new SortingDatasetModel("Quick Sort");
